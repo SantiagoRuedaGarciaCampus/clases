88aec8e8e2b9558dacdb87ded5e0d6230c1f38df
SantiagoRuedaGarciaCampus <130388343+SantiagoRuedaGarciaCampus@users.noreply.github.com>
2023-10-02 12:36:33 -0500

    Clase 4
    npm i to install node_modules

---
+++ clase 4/ejercicio/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const  Home = () =>{
+    return(
+        <div>
+            <h1>Canasta Familiar</h1>
+            <p>
+                Selecciona la accion deseada
+            </p>
+            <div>
+                <a placeholder="Calificanos" href="https://www.google.com/search?channel=fs&client=ubuntu-sn&q=imagen"/>
+            </div>
+        </div>
+    )
+}
+
+export default Home
